38913faa0e9fae9f4970981da79df14e9f785364
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-06-07 23:57:38 +0400

    update

---
+++ contracts/BBBFarmer.sol
@@ -0,0 +1,71 @@
+pragma solidity =0.8.23;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PointToken is Ownable, ERC20Burnable {
+    constructor(
+        string memory name,
+        string memory symbol
+    ) Ownable(msg.sender) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
+
+contract BBBFarmer {
+    address public pointToken;
+
+    address public bbb;
+
+    uint256 public price;
+
+    struct User {
+        uint256 stake;
+        uint256 last;
+        uint256 point;
+    }
+
+    mapping(address => User) public users;
+
+    address[] public userAddrs;
+
+    constructor() {
+        bbb = 0xFa4dDcFa8E3d0475f544d0de469277CF6e0A6Fd1;
+        price = 257e18;
+        pointToken = address(new PointToken("Carrot", "Carrot"));
+    }
+
+    function getUserAddrsLength() external view returns (uint256) {
+        return userAddrs.length;
+    }
+
+    function buy(uint256 amt) external {
+        _sync(msg.sender);
+        ERC20Burnable(bbb).burnFrom(msg.sender, amt * price);
+        users[msg.sender].stake += amt;
+    }
+
+    function collect() external {
+        _sync(msg.sender);
+        User storage user = users[msg.sender];
+        uint256 point = user.point;
+        user.point = 0;
+        PointToken(pointToken).mint(msg.sender, point);
+    }
+
+    function _sync(address addr) private {
+        User storage user = users[addr];
+        if (user.last == 0) {
+            userAddrs.push(addr);
+        }
+        user.point += user.stake * (block.number - user.last);
+        user.last = block.number;
+    }
+
+    function getPendingPoint(address addr) external view returns (uint256) {
+        User memory user = users[addr];
+        return user.point + user.stake * (block.number - user.last);
+    }
+}
