588eefd526bf716dd59f658ad9447349f4896caa
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-06-13 00:03:51 +0400

    Create BBB.sol

---
+++ contracts/BBB.sol
@@ -0,0 +1,13 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity =0.8.19;
+
+// import {ERC20Burnable, ERC20} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+// import {Ownable} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/access/Ownable.sol";
+
+// contract BBB is ERC20Burnable {
+//     constructor(string memory name_, string memory symbol_)
+//         ERC20(name_, symbol_)
+//     {
+//         _mint(msg.sender, 1e10 ether);
+//     }
+// }
